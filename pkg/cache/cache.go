f180796e5dde4f8b2b923e050719c65a352aab25
Julian Liu <wlootlxt123@gmail.com>
2022-07-04 19:37:21 +0800

    i

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -91,11 +92,36 @@ func TryGet(k string, or func() string) string {
 	s := or()
 	if s != "" {
 		new := filepath.Join(fn, filepath.Base(s))
-		err = os.Rename(s, new)
+		err = moveFile(s, new)
 		if err == nil {
 			return new
 		}
 		fmt.Printf("cache: save failed %v\n", err)
 	}
 	return s
+}
+
+// for docker
+func moveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+	return nil
 }
