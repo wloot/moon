93e2f661ded8119566d8a21e733186d099d2da22
Julian Liu <wlootlxt123@gmail.com>
2022-07-26 12:58:54 +0800

    i

--- cmd/moon/main.go
+++ cmd/moon/main.go
@@ -327,14 +327,7 @@ func writeSub(subFiles []string, v emby.EmbyVideo) (bool, error) {
 					// 一个常见的字幕typo
 					data = bytes.Replace(data, []byte(",&H00H202020,"), []byte(",&H00202020,"), 1)
 					data = bytes.Replace(data, []byte("[Aegisub Project Garbage]"), []byte(""), 1)
-					s, err = astisub.ReadFromSSAWithOptions(bytes.NewReader(data), astisub.SSAOptions{
-						OnUnknownSectionName: func(name string) {
-							fmt.Printf("astisub: unknown section: %s", name)
-						},
-						OnInvalidLine: func(line string) {
-							//fmt.Printf("astisub: not understood: '%s', ignoring", line)
-						},
-					})
+					s, err = astisub.ReadFromSSAWithOptions(bytes.NewReader(data), astisub.SSAOptions{})
 				}
 				if ext == "srt" {
 					s, err = astisub.ReadFromSRT(bytes.NewReader(data))
