5bda6f7f5313f7c5fad3f7f9e1c1eb45227f28fd
Julian Liu <wlootlxt123@gmail.com>
2022-08-26 06:13:32 +0800

    i

--- cmd/moon/main.go
+++ cmd/moon/main.go
@@ -320,9 +320,9 @@ func writeSub(subFiles []string, v emby.EmbyVideo) (bool, error) {
 		fmt.Printf("processing raw file %v\n", subName)
 		err := unpack.WalkUnpacked(subName, func(reader io.Reader, info fs.FileInfo) {
 			name := info.Name()
-			//if utf8, err := charset.AnyToUTF8([]byte(name)); err == nil {
-			//	name = string(utf8)
-			//}
+			if utf8, err := charset.AnyToUTF8([]byte(name)); err == nil {
+				name = string(utf8)
+			}
 			if v.Type == "Episode" {
 				if filepath.Base(name) != filepath.Base(subName) {
 					ep := episode.NameToEpisode(name)
