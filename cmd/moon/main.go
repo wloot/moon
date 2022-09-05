787f0246401330df52af0ebbdd36cfb136967be6
Julian Liu <wlootlxt123@gmail.com>
2022-09-05 14:39:33 +0800

    i

--- cmd/moon/main.go
+++ cmd/moon/main.go
@@ -327,7 +327,7 @@ func writeSub(subFiles []string, v emby.EmbyVideo) (bool, error) {
 			if v.Type == "Episode" {
 				if filepath.Base(name) != filepath.Base(subName) {
 					ep := episode.NameToEpisode(name)
-					if ep <= 0 || ep != v.IndexNumber {
+					if ep >= 0 && v.IndexNumber != ep {
 						//fmt.Printf("skip file %v as ep number not match %v\n", name, v.IndexNumber)
 						return
 					}
