3efcb13c9641e0057c47f0aa9213bf2d544f8de2
Julian Liu <wlootlxt123@gmail.com>
2022-09-09 05:51:26 +0800

    i

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -23,6 +23,7 @@ const (
 func updateAccess(dir string) {
 	f, err := os.OpenFile(filepath.Join(dir, nameAccess), os.O_CREATE|os.O_WRONLY, 0755)
 	if err == nil {
+		defer f.Close()
 		f.WriteString(strconv.FormatInt(time.Now().Unix(), 10))
 	}
 }
