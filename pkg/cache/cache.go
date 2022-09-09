4e8197a5b0d1e3ee62e768e144d1c28bc766b5e6
Julian Liu <wlootlxt123@gmail.com>
2022-09-09 15:59:06 +0800

    i

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -132,23 +132,23 @@ func TryGet(k string, sub string, or func() string) string {
 func moveFile(sourcePath, destPath string) error {
 	inputFile, err := os.Open(sourcePath)
 	if err != nil {
-		return fmt.Errorf("Couldn't open source file: %s", err)
+		return err
 	}
 	outputFile, err := os.Create(destPath)
 	if err != nil {
 		inputFile.Close()
-		return fmt.Errorf("Couldn't open dest file: %s", err)
+		return err
 	}
 	defer outputFile.Close()
 	_, err = io.Copy(outputFile, inputFile)
 	inputFile.Close()
 	if err != nil {
-		return fmt.Errorf("Writing to output file failed: %s", err)
+		return err
 	}
 	// The copy was successful, so now delete the original file
 	err = os.Remove(sourcePath)
 	if err != nil {
-		return fmt.Errorf("Failed removing original file: %s", err)
+		return err
 	}
 	return nil
 }
